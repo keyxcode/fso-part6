39d30db7f6d4fc18146a2f71f003f6a2cb5c9571
keyxcode <keyxcode@gmail.com>
2023-04-17 17:15:41 +0700

    Add filter component

---
+++ redux-anecdotes/src/components/Filter.js
@@ -0,0 +1,14 @@
+const Filter = () => {
+  const handleChange = (event) => {};
+  const style = {
+    marginBottom: 10,
+  };
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Filter;
