3ea6a56972d2efba5dd3f468b2ccde40c25e77f4
keyxcode <keyxcode@gmail.com>
2023-04-18 12:32:22 +0700

    Update notification on adding a post

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import { notificationChange } from "../reducers/notificationReducer";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
@@ -11,6 +12,7 @@ const AnecdoteForm = () => {
     event.target.anecdote.value = "";
 
     dispatch(createAnecdote(content));
+    dispatch(notificationChange(`you added ${content}`));
   };
 
   return (
